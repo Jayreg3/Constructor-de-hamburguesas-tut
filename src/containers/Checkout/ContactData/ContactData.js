50f3930f0db8c51a0e166db68e03200447b063fa
Jayreg3 <jeregistre@gmail.com>
2020-02-15 22:40:06 +0100

    Termine 272-280; conecte el formulario con el baso de datos de firebase para que los usarios puede subir informacion de contacto para sus pedidos

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -14,7 +14,12 @@ class ContactData extends Component {
           type: "text",
           placeholder: "Tú nombre"
         },
-        value: ""
+        value: "",
+        validation: {
+          required: true
+        },
+        valid: false,
+        touched: false
       },
       calle: {
         elementType: "input",
@@ -22,7 +27,12 @@ class ContactData extends Component {
           type: "text",
           placeholder: "Calle"
         },
-        value: ""
+        value: "",
+        validation: {
+          required: true
+        },
+        valid: false,
+        touched: false
       },
       ciudad: {
         elementType: "input",
@@ -30,7 +40,12 @@ class ContactData extends Component {
           type: "text",
           placeholder: "Ciudad"
         },
-        value: ""
+        value: "",
+        validation: {
+          required: true
+        },
+        valid: false,
+        touched: false
       },
       código_postal: {
         elementType: "input",
@@ -38,7 +53,14 @@ class ContactData extends Component {
           type: "text",
           placeholder: "Código Postal"
         },
-        value: ""
+        value: "",
+        validation: {
+          required: true,
+          minLength: 5,
+          maxLength: 5
+        },
+        valid: false,
+        touched: false
       },
       país: {
         elementType: "input",
@@ -46,7 +68,12 @@ class ContactData extends Component {
           type: "text",
           placeholder: "País"
         },
-        value: ""
+        value: "",
+        validation: {
+          required: true
+        },
+        valid: false,
+        touched: false
       },
       correo: {
         elementType: "input",
@@ -54,7 +81,12 @@ class ContactData extends Component {
           type: "email",
           placeholder: "Tú Correo"
         },
-        value: ""
+        value: "",
+        validation: {
+          required: true
+        },
+        valid: false,
+        touched: false
       },
       método_de_entrega: {
         elementType: "select",
@@ -64,20 +96,30 @@ class ContactData extends Component {
             { value: "más_barato", displayValue: "Más Barato" }
           ]
         },
-        value: ""
+        value: "más_rapido",
+        validation: {},
+        valid: true
       }
     },
+    formIsValid: false,
     loading: false
   };
 
   orderHandler = event => {
     event.preventDefault();
     console.log(this.props.ingredients);
+    const formData = {};
+    for (let formElementIdentifier in this.state.orderForm) {
+      formData[formElementIdentifier] = this.state.orderForm[
+        formElementIdentifier
+      ].value;
+    }
     this.setState({ loading: true });
     const order = {
       ingredientes: this.props.ingredients,
       //en realidad, debes re-calcular el precio por si acaso un usuario ha manipulado el precio manualmente
-      precio: this.props.price
+      precio: this.props.price,
+      orderData: formData
     };
     axios
       .post("/orders.json", order)
@@ -92,6 +134,24 @@ class ContactData extends Component {
       });
   };
 
+  checkValidity(value, rules) {
+    let isValid = true;
+
+    if (rules.required) {
+      isValid = value.trim() !== "" && isValid;
+    }
+
+    if (rules.minLength) {
+      isValid = value.length >= rules.minLength && isValid;
+    }
+
+    if (rules.maxLength) {
+      isValid = value.length <= rules.minLength && isValid;
+    }
+
+    return isValid;
+  }
+
   inputChangedHandler = (event, inputIdentifier) => {
     console.log(event.target.value);
     const updatedOrderForm = {
@@ -99,8 +159,20 @@ class ContactData extends Component {
     };
     const updatedFormElement = { ...updatedOrderForm[inputIdentifier] };
     updatedFormElement.value = event.target.value;
+    updatedFormElement.valid = this.checkValidity(
+      updatedFormElement.value,
+      updatedFormElement.validation
+    );
+    console.log(updatedFormElement);
+    updatedFormElement.touched = true;
     updatedOrderForm[inputIdentifier] = updatedFormElement;
-    this.setState({ orderForm: updatedOrderForm });
+
+    let formIsValid = true;
+    for (let inputIdentifier in updatedOrderForm) {
+      formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid;
+    }
+
+    this.setState({ orderForm: updatedOrderForm, formIsValid: formIsValid });
   };
 
   render() {
@@ -109,7 +181,7 @@ class ContactData extends Component {
       formElementsArray.push({ id: key, config: this.state.orderForm[key] });
     }
     let form = (
-      <form>
+      <form onSubmit={this.orderHandler}>
         {formElementsArray.map(formElement => {
           return (
             <Input
@@ -117,11 +189,14 @@ class ContactData extends Component {
               elementType={formElement.config.elementType}
               elementConfig={formElement.config.elementConfig}
               value={formElement.config.value}
+              invalid={!formElement.config.valid}
+              shouldValidate={formElement.config.validation}
+              touched={formElement.config.touched}
               changed={event => this.inputChangedHandler(event, formElement.id)}
             />
           );
         })}
-        <Button btnType="Success" clicked={this.orderHandler}>
+        <Button btnType="Success" disabled={!this.state.formIsValid}>
           Pide
         </Button>
       </form>
